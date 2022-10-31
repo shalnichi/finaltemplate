bc1b65bbd74addc8837d6979f10691a703bda5b8
Faishal Abyan Murtadho <famurtadho@gmail.como>
2022-10-31 09:16:09 +0700

    template baru

---
+++ index.js
@@ -0,0 +1,22 @@
+var carouselWidth = $('.carousel-inner')[0].scrollWidth;
+var cardWidth = $('.carousel-item').width();
+
+var scrollPosition = 0;
+
+$('.carousel-control-next').on('click', function(){
+    if(scrollPosition < (carouselWidth - (cardWidth *4))){
+        console.log('next');
+        scrollPosition = scrollPosition + cardWidth;
+        $('.carousel-inner').animate({scrolleft: scrollPosition},600);
+    }
+});
+
+$('.carousel-control-prev').on('click', function(){
+    if(scrollPosition > 0{
+        console.log('prev');
+        scrollPosition = scrollPosition + cardWidth;
+        $('.carousel-inner').animate({scrolleft: scrollPosition},600);
+    }
+});
+
+
